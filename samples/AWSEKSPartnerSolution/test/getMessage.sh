ee14f9b8b0f79503211e961396a1ade8df2e0cfa
Callum Jackson <callumj@uk.ibm.com>
2023-03-06 14:39:39 +0000

    Updates for 9.3.2, LB annotations and seccomp

---
+++ samples/AWSEKSPartnerSolution/test/getMessage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# © Copyright IBM Corporation 2023
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+export TARGET_NAMESPACE=${1:-"default"}
+export MQCCDTURL="${DIR}/ccdt_generated.json"
+export MQSSLKEYR="${DIR}/../../genericresources/createcerts/application"
+
+export PORT="1414"
+export IPADDRESS="$(kubectl get service secureapphelm-ibm-mq-loadbalancer -o jsonpath='{..hostname}')"
+
+( echo "cat <<EOF" ; cat ccdt_template.json ; echo EOF ) | sh > ccdt_generated.json
+
+
+echo "Starting amqsghac" secureapphelm
+/opt/mqm/samp/bin/amqsghac APPQ secureapphelm
